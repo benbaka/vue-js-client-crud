3603d45b2152698736456b9a9ffa5c756c3a07b1
Benjamin Baka <baka@mpedigree.net>
2020-08-15 20:07:53 +0000

    Ben: make input fields more reactive

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -9,7 +9,7 @@
                         <form @submit.prevent="handleSubmit">
                             <div class="form-group ">
                                 <label for="name"><b>Name</b></label>
-                                <input type="text" v-model="task.name" id="name" name="name" class="form-control" :class="{ 'animated headShake  is-invalid':  $v.task.name.$error }" />
+                                <input type="text" v-model="task.name" id="name" name="name" @keyup="$v.task.name.$touch()" class="form-control" :class="{ 'animated headShake  is-invalid':  $v.task.name.$error }" />
                                 <div v-if="!$v.task.name.required" class="invalid-feedback">
                                     <span>Name is required</span>
                                 </div>
@@ -18,7 +18,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="name"><b>Description</b></label>
-                                <input type="text" v-model="task.description" name="name" class="form-control" :class="{ 'animated headShake is-invalid':  $v.task.description.$error }" />
+                                <input type="text" v-model="task.description" name="name" @keyup="$v.task.description.$touch()" class="form-control" :class="{ 'animated headShake is-invalid':  $v.task.description.$error }" />
                                 <div v-if=" !$v.task.description.required" class="invalid-feedback">
                                     <span>Description is required</span>
                                 </div>
@@ -36,8 +36,8 @@
                             <br><br>
                             <div class="form-group">
                                 <label for="testAnswer">Test Question: <b>{{testQuestion}}</b></label>
-                                <input type="text" v-model="testAnswer" name="testAnswer" class="form-control" :class="{'animated headShake is-invalid': $v.testAnswer.$error}"/>
-                                <div v-if="!$v.testAnswer.required" class="invalid-feedback"><span>Evaluate the expression and put in the correct answer</span></div>
+                                <input type="text" v-model="testAnswer" name="testAnswer" @keyup="$v.testAnswer.$touch()" class="form-control" :class="{'animated headShake is-invalid': $v.testAnswer.$error}"/>
+                                <div v-if="!$v.testAnswer.checkAnswer" class="invalid-feedback"><span>Evaluate the expression and put in the correct answer</span></div>
                                 <div v-if="!$v.testAnswer.integer" class="invalid-feedback"><span>Answer must be a number</span></div>
                             </div>
 
@@ -61,7 +61,7 @@
 
     // Custom validations
     function checkAnswer(){
-        if(parseInt(this.testAnswer)===5){
+        if(parseInt(this.testAnswer)===6){
             return true
         }
         else{
